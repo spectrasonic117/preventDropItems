637a71354341579875bd8d0922fc30c84b602769
Manuel Heredia <Spectrasonic117@gmail.com>
2024-11-07 13:29:31 -0600

    1.20.4 version

--- src/main/java/com/spectrasonic/preventDropItems/Commands/PreventDropCommand.java
+++ src/main/java/com/spectrasonic/preventDropItems/Commands/PreventDropCommand.java
@@ -11,6 +11,7 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("all")
 public class PreventDropCommand implements CommandExecutor, TabCompleter {
 
     private final Main plugin;
